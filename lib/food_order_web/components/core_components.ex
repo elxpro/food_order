76afb98627db88e91c53d48cdc0787058733d78d
Gustavo <g.92oliveira@gmail.com>
2022-12-20 07:05:53 -0300

    format and Credo

--- lib/food_order_web/components/core_components.ex
+++ lib/food_order_web/components/core_components.ex
@@ -11,6 +11,8 @@ defmodule FoodOrderWeb.CoreComponents do
   """
   use Phoenix.Component
 
+  alias Phoenix.HTML
+  alias Phoenix.HTML.Form
   alias Phoenix.LiveView.JS
   import FoodOrderWeb.Gettext
 
@@ -234,7 +236,7 @@ defmodule FoodOrderWeb.CoreComponents do
   @doc """
   Renders an input with label and error messages.
 
-  A `%Phoenix.HTML.Form{}` and field name may be passed to the input
+  A `%Form{}` and field name may be passed to the input
   to build input names and error messages, or all the attributes and
   errors may be passed explicitly.
 
@@ -253,11 +255,11 @@ defmodule FoodOrderWeb.CoreComponents do
                range radio search select tel text textarea time url week)
 
   attr :value, :any
-  attr :field, :any, doc: "a %Phoenix.HTML.Form{}/field name tuple, for example: {f, :email}"
+  attr :field, :any, doc: "a %Form{}/field name tuple, for example: {f, :email}"
   attr :errors, :list
   attr :checked, :boolean, doc: "the checked flag for checkbox inputs"
   attr :prompt, :string, default: nil, doc: "the prompt for select inputs"
-  attr :options, :list, doc: "the options to pass to Phoenix.HTML.Form.options_for_select/2"
+  attr :options, :list, doc: "the options to pass to Form.options_for_select/2"
   attr :multiple, :boolean, default: false, doc: "the multiple flag for select inputs"
   attr :rest, :global, include: ~w(autocomplete disabled form max maxlength min minlength
                                    pattern placeholder readonly required size step)
@@ -267,11 +269,11 @@ defmodule FoodOrderWeb.CoreComponents do
     assigns
     |> assign(field: nil)
     |> assign_new(:name, fn ->
-      name = Phoenix.HTML.Form.input_name(f, field)
+      name = Form.input_name(f, field)
       if assigns.multiple, do: name <> "[]", else: name
     end)
-    |> assign_new(:id, fn -> Phoenix.HTML.Form.input_id(f, field) end)
-    |> assign_new(:value, fn -> Phoenix.HTML.Form.input_value(f, field) end)
+    |> assign_new(:id, fn -> Form.input_id(f, field) end)
+    |> assign_new(:value, fn -> Form.input_value(f, field) end)
     |> assign_new(:errors, fn -> translate_errors(f.errors || [], field) end)
     |> input()
   end
@@ -308,7 +310,7 @@ defmodule FoodOrderWeb.CoreComponents do
         {@rest}
       >
         <option :if={@prompt}><%= @prompt %></option>
-        <%= Phoenix.HTML.Form.options_for_select(@options, @value) %>
+        <%= Form.options_for_select(@options, @value) %>
       </select>
       <.error :for={msg <- @errors}><%= msg %></.error>
     </div>
@@ -618,6 +620,6 @@ defmodule FoodOrderWeb.CoreComponents do
   end
 
   defp input_equals?(val1, val2) do
-    Phoenix.HTML.html_escape(val1) == Phoenix.HTML.html_escape(val2)
+    HTML.html_escape(val1) == HTML.html_escape(val2)
   end
 end
