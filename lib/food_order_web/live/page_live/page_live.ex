033d2a88a4b28b7ddfe0151edf4438265c9b6a52
Gustavo <g.92oliveira@gmail.com>
2022-12-21 06:59:09 -0300

    most common lifecycle livevieo

--- lib/food_order_web/live/page_live/page_live.ex
+++ lib/food_order_web/live/page_live/page_live.ex
@@ -1,9 +1,25 @@
 defmodule FoodOrderWeb.PageLive do
   use FoodOrderWeb, :live_view
 
+  def mount(_, _, socket) do
+    {:ok, assign(socket, name: "hi")}
+  end
+
   def render(assigns) do
     ~H"""
-        hi page
+        <%= self() |> :erlang.pid_to_list() %>
+        hi <%= @name %>
     """
   end
+
+  def handle_info({:change_name, name}, socket) do
+    {:noreply, assign(socket, name: name)}
+  end
+
+  # mount
+  # handle_params
+  # attach_hook - in the future
+  # handle_info
+  # handle_event
+  # components -
 end
