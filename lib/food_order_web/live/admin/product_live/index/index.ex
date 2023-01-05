a9cb1909d4e53c828524f1ee2ef54c7d88e7ff76
Gustavo <g.92oliveira@gmail.com>
2023-01-05 09:07:12 -0300

    edit product

--- lib/food_order_web/live/admin/product_live/index/index.ex
+++ lib/food_order_web/live/admin/product_live/index/index.ex
@@ -9,9 +9,9 @@ defmodule FoodOrderWeb.Admin.ProductLive.Index do
     {:ok, assign(socket, products: products)}
   end
 
-  def handle_params(_params, _, socket) do
+  def handle_params(params, _, socket) do
     live_action = socket.assigns.live_action
-    {:noreply, apply_action(socket, live_action)}
+    {:noreply, apply_action(socket, live_action, params)}
   end
 
   def handle_event("delete", %{"id" => id}, socket) do
@@ -22,11 +22,16 @@ defmodule FoodOrderWeb.Admin.ProductLive.Index do
     {:noreply, update(socket, :products, &delete_product.(&1, id))}
   end
 
-  defp apply_action(socket, :new) do
+  defp apply_action(socket, :new, _params) do
     socket |> assign(:page_title, "New Product") |> assign(:product, %Product{})
   end
 
-  defp apply_action(socket, :index) do
+  defp apply_action(socket, :edit, %{"id" => id}) do
+    product = Products.get_product!(id)
+    socket |> assign(:page_title, "Edit Product") |> assign(:product, product)
+  end
+
+  defp apply_action(socket, :index, _params) do
     socket |> assign(:page_title, "List Products")
   end
 end
--- lib/food_order_web/live/admin/product_live/show/show.ex
+++ lib/food_order_web/live/admin/product_live/show/show.ex
@@ -1,9 +1,10 @@
 defmodule FoodOrderWeb.Admin.ProductLive.Show do
   use FoodOrderWeb, :live_view
   alias FoodOrder.Products
+  alias FoodOrderWeb.Admin.ProductLive.Form
 
   def handle_params(%{"id" => id}, _, socket) do
     product = Products.get_product!(id)
-    {:noreply, assign(socket, :product, product)}
+    {:noreply, assign(socket, :product, product) |> assign(page_title: "Product #{product.id}")}
   end
 end
