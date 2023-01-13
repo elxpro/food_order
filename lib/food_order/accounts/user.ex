382c6267d5318b43e56185d58c1dacb0dcef7a3a
Gustavo <g.92oliveira@gmail.com>
2023-01-13 08:17:06 -0300

    seeds

--- lib/food_order/accounts/user.ex
+++ lib/food_order/accounts/user.ex
@@ -39,7 +39,7 @@ defmodule FoodOrder.Accounts.User do
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
+    |> cast(attrs, [:email, :password, :role])
     |> validate_email(opts)
     |> validate_password(opts)
   end
@@ -58,7 +58,9 @@ defmodule FoodOrder.Accounts.User do
     |> validate_length(:password, min: 12, max: 72)
     |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
     |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
-    |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
+    |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/,
+      message: "at least one digit or punctuation character"
+    )
     |> maybe_hash_password(opts)
   end
 
