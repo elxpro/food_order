2a3bf878c00ac2a54b2f551c4d48b21ee83ba5e2
Gustavo <g.92oliveira@gmail.com>
2023-01-16 14:46:48 -0300

    fix test

--- lib/food_order_web/live/middlewares/require_admin.ex
+++ lib/food_order_web/live/middlewares/require_admin.ex
@@ -3,6 +3,7 @@ defmodule FoodOrderWeb.RequireAdmin do
 
   def on_mount(_default, _params, _session, socket) do
     current_user = socket.assigns.current_user
+
     if current_user.role == :ADMIN do
       {:cont, socket}
     else
