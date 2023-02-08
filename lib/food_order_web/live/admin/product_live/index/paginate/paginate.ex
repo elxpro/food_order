3724cd1ca60e4fdd11d5a5d7e25b2305deeb0f4e
Gustavo <g.92oliveira@gmail.com>
2023-02-08 06:40:18 -0300

    format

--- lib/food_order_web/live/admin/product_live/index/paginate/paginate.ex
+++ lib/food_order_web/live/admin/product_live/index/paginate/paginate.ex
@@ -4,14 +4,24 @@ defmodule FoodOrderWeb.Admin.ProductLive.Index.Paginate do
   def render(assigns) do
     ~H"""
     <div id={@id} class="flex pr-4">
-      <div :if={@options.page > 1} class="h-8 w-8 mr-1 flex justify-center items center cursor-pointer">
+      <div
+        :if={@options.page > 1}
+        class="h-8 w-8 mr-1 flex justify-center items center cursor-pointer"
+      >
         <.link patch={~p"/admin/products?#{Map.update(@options, :page, @options.page, &(&1 - 1))}"}>
           <Heroicons.backward solid class="h-6 w-6 text-orange-500 stroke-current" />
         </.link>
       </div>
 
       <div class="flex h-8 font-medium">
-        <div :for={current_page <- (@options.page - 2)..(@options.page + 1)} :if={current_page > 0} class={[current_page ==  @options.page && "border-orange-500" || "border-transparent", "w-8 md:flex justify-center items-center hidden cursor-pointer leading-5 transition duration-150 ease-in border-b-2"]}>
+        <div
+          :for={current_page <- (@options.page - 2)..(@options.page + 1)}
+          :if={current_page > 0}
+          class={[
+            (current_page == @options.page && "border-orange-500") || "border-transparent",
+            "w-8 md:flex justify-center items-center hidden cursor-pointer leading-5 transition duration-150 ease-in border-b-2"
+          ]}
+        >
           <.link patch={~p"/admin/products?#{Map.put(@options, :page, current_page)}"}>
             <%= current_page %>
           </.link>
