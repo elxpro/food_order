6a91aa2a7a72ffe6df28ca376da49325f645215e
Gustavo <g.92oliveira@gmail.com>
2023-02-08 07:10:38 -0300

    sdfsadf

--- test/food_order_web/live/admin/product_live/index/paginate_test.exs
+++ test/food_order_web/live/admin/product_live/index/paginate_test.exs
@@ -15,26 +15,39 @@ defmodule FoodOrderWeb.Admin.ProductLive.Index.PaginateTest do
       |> element("[data-role=next]")
       |> render_click()
 
-      assert_patched(view, ~p"/admin/products?name=&page=2&per_page=4&sort_by=updated_at&sort_order=desc")
+      assert_patched(
+        view,
+        ~p"/admin/products?name=&page=2&per_page=4&sort_by=updated_at&sort_order=desc"
+      )
 
       view
       |> element("[data-role=previous]")
       |> render_click()
 
-      assert_patched(view, ~p"/admin/products?name=&page=1&per_page=4&sort_by=updated_at&sort_order=desc")
+      assert_patched(
+        view,
+        ~p"/admin/products?name=&page=1&per_page=4&sort_by=updated_at&sort_order=desc"
+      )
 
       view
       |> element("#pages>div>div>a", "2")
       |> render_click()
 
-      assert_patched(view, ~p"/admin/products?name=&page=2&per_page=4&sort_by=updated_at&sort_order=desc")
+      assert_patched(
+        view,
+        ~p"/admin/products?name=&page=2&per_page=4&sort_by=updated_at&sort_order=desc"
+      )
     end
 
     test "using params", %{conn: conn} do
       product_1 = product_fixture(name: "pumpkin")
       product_2 = product_fixture(name: "pumpkin 2")
 
-      {:ok, view, _html} = live(conn, ~p"/admin/products?name=&page=1&per_page=1&sort_by=inserted_at&sort_order=desc")
+      {:ok, view, _html} =
+        live(
+          conn,
+          ~p"/admin/products?name=&page=1&per_page=1&sort_by=inserted_at&sort_order=desc"
+        )
 
       assert has_element?(view, "#products-#{product_1.id}")
       refute has_element?(view, "#products-#{product_2.id}")
@@ -48,6 +61,5 @@ defmodule FoodOrderWeb.Admin.ProductLive.Index.PaginateTest do
       assert has_element?(view, "#products-#{product_2.id}")
       refute has_element?(view, "#products-#{product_1.id}")
     end
-
   end
 end
